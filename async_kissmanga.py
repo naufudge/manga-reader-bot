befb6c2f2e9186b49f5cf2c0541ae7205d5fb20b
naufudge <130215373+naufudge@users.noreply.github.com>
2023-04-09 00:10:15 +0500

    added "mangaInfo" function and similarity sorting
    Added a function (search_results_sorter) which will sort the search results based on the similarity between it and the search keyword. Moreover, added some documentation which explains the functions a little bit.

--- async_kissmanga.py
+++ async_kissmanga.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import httpx
 import asyncio
+from difflib import SequenceMatcher
 
 def no_cap_dict(d):
     """
@@ -11,6 +12,12 @@ def no_cap_dict(d):
         new_d[x.lower()] = y
     return new_d
 
+def similar(a, b):
+    """
+    Similarity check between ``a`` and ``b``. Returns a ratio (Float).
+    """
+    return SequenceMatcher(None, a, b).ratio()
+
 class Async_KissManga():
     def __init__(self):
         self.home = "https://kissmanga.org"
@@ -20,6 +27,16 @@ class Async_KissManga():
         self.chapters = []
 
     async def search_page_loader(self, client, url, page_num):
+        """
+        Scrapes the search result page.
+        ### Parameters
+        ``client``: httpx.Client
+            The httpx client that you are using.
+        ``url``: str
+            The URL of the search result page.
+        ``page_num``: int
+            The search result page number.
+        """
         page = await client.get(url)
         if page.status_code < 400:
             page_soup = BeautifulSoup(page.content, 'html.parser')
@@ -42,6 +59,13 @@ class Async_KissManga():
         return
 
     async def Search(self, keyword):
+        """
+        Search for any manga that you want.\n
+        ### Parameters
+        ``keyword``: str
+            The name of the manga you want to search \n
+        This function will return a dictionary of the results containing the name of the manga as key and the link as the value.
+        """
         test_p, page_urls, tasks = 10, [], []
         async with httpx.AsyncClient(timeout=self.timeout) as client:
             for j in range(1, test_p):
@@ -52,9 +76,43 @@ class Async_KissManga():
             tests = await asyncio.gather(*tasks)
         return self.search_results
 
-    def Chapters(self, manga_link):
+    def search_results_sorter(self, search_results, keyword):
+        """
+        Sorts the search results based on the similarity between it and the keyword.
+        ### Parameters
+        ``search_results``: sequence
+            A non-empty sequence of search results. \n
+        ``keyword``: str
+            The name of the manga you searched. \n
+        Returns the sorted list.
+        """
+        temp_listed_results = []
+        for result in search_results:
+            x = similar(keyword, result)
+            temp_listed_results.append((x, result))
+
+        temp_listed_results.sort(reverse=True)
+        sorted_results = [x[1] for x in temp_listed_results]
+
+        return sorted_results
+
+    def load_managa_page(self, manga_link):
+        """
+        Loads up the specific manga page. Returns manga page's HTML source code.
+        """
         manga_page = self.client.get(manga_link)
-        manga_soup = BeautifulSoup(manga_page, 'html.parser')
+        soup = BeautifulSoup(manga_page, 'html.parser')
+        return soup
+
+    def Chapters(self, manga_link):
+        """
+        Find the all the chapters of a specific manga.\n
+        ### Parameters
+        ``manga_link``: str
+            The link of the manga \n
+        This function will return a list of tuples, where the first value of the tuple is the name of the chapter and the second value of the tuple is the link of the chapter.
+        """
+        manga_soup = self.load_managa_page(manga_link)
 
         parent_chapter_list = manga_soup.find('div', class_='listing listing8515 full')
         chapter_list = parent_chapter_list.find_all('h3')
@@ -67,10 +125,45 @@ class Async_KissManga():
 
             self.chapters.append((chapter, f"{self.home}{chapter_url}"))
 
-        self.chapters.reverse() # Asscending order
+        self.chapters.reverse() # Changes to asscending order
         return self.chapters
 
+    def mangaInfo(self, manga_link):
+        """
+        Find the basic info of a specific manga.
+        ### Parameters
+        ``manga_link``: str
+            The link of the manga \n
+        This function will return a dictionary with the 'title', 'other name', 'authors', 'summary' and 'cover' of the manga.
+        """
+        manga_soup = self.load_managa_page(manga_link)
+        info = {}
+        main_window = manga_soup.find('div', class_='barContent full')
+        details = main_window.find('div', class_='full')
+        other_info = details.find_all('p', class_='info')
+
+        manga_cover_parent = manga_soup.find('div', class_='barContent cover_anime full')
+        manga_cover = manga_cover_parent.find('img')['src']
+
+        info['title'] = details.find('h2').text.strip()
+        for each in other_info:
+            span = each.find('span').text.strip().lower()
+            if span != 'genres:':
+                a = each.find('a').text.strip()
+                info[span.replace(':', '')] = a
+        info['summary'] = details.find('div', class_='summary').text.strip()
+        info['cover'] = f"{self.home}{manga_cover}"
+
+        return info
+
     def read_chap(self, chap_url):
+        """
+        Find the images of the pages of a specific chapter.
+        ### Parameters
+        ``chap_url``: str
+            URL of the chapter.
+        This function returns a list of image URLs in order.
+        """
         chapter_page = self.client.get(chap_url)
         chapter_soup = BeautifulSoup(chapter_page.content, 'html.parser')
 
